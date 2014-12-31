0384ae7c4ab17963cc12155dd7fdcf3de2005868
ybliu_hf <ybliu_hf@ingenic.cn>
2014-12-31 10:58:25 +0800

    poj 1002

--- 487-3279.cpp
+++ 487-3279.cpp
@@ -16,68 +16,73 @@ using namespace std;
 
 int main()
 {
-	int n = 0, num_pri = 0, i = 0, j = 0, k = 0, num_flag = 0, multiple = 1, tmp = 0;
+	int n = 0, i = 0, j = 0, k = 0, num_flag = 0, multiple = 1, tmp = 0;
 	scanf("%d", &n);
-//	printf("%d", n);
-//	cin >> n;
-//	cout << n << endl;
-	int phone[n][7];
-	int flag[1000] = {0}, num_sort[1000] = {0};
-	char num[20];
+	int phone[100000][8];
+	int flag[100000] = {0}, num_sort[100000] = {0};
+	char num[2000];
 
-	num_pri = n;
 	for (i = 0; i < n; i++) {
 		scanf("%s", num);
-//		cin >> num;
-		for (j = 0, k = 0; j < strlen(num); j++) {
+		multiple = 1;
+		for (j = strlen(num) - 1, k = 0; j >= 0; j--) {
 			if (num[j] <= '9' && num[j] >= '0') {
 				phone[i][k] = num[j] - 48;
-				debug_487("%d %d %d\n", __LINE__, phone[i][k], num[j] - 48);
+				num_sort[i] += phone[i][k] * multiple;
+				multiple *= 10;
+				debug_487("%d %d %d\n", phone[i][k], num[j] - 48, num_sort[i]);
 				k++;
 			} else if (num[j] <= 'O' && num[j] >= 'A') {
 				phone[i][k] = (num[j] - 'A') / 3 + 2;
-				debug_487("%d %d %d\n", __LINE__, phone[i][k], num[j] - 65);
+				num_sort[i] += phone[i][k] * multiple;
+				multiple *= 10;
+				debug_487("%d %d %d\n", phone[i][k], num[j] - 65, num_sort[i]);
 				k++;
 			} else if (num[j] <= 'S' && num[j] >= 'P') {
 				phone[i][k] = 7;
-				debug_487("%d %d %d\n", __LINE__, phone[i][k], num[j] - 65);
+				num_sort[i] += phone[i][k] * multiple;
+				multiple *= 10;
+				debug_487("%d %d %d\n", phone[i][k], num[j] - 65, num_sort[i]);
 				k++;
 			} else if (num[j] <= 'Y' && num[j] >= 'T') {
 				phone[i][k] = (num[j] - 'T') / 3 + 8;
-				debug_487("%d %d %d\n", __LINE__, phone[i][k], num[j] - 65);
+				num_sort[i] += phone[i][k] * multiple;
+				multiple *= 10;
+				debug_487("%d %d %d\n", phone[i][k], num[j] - 65, num_sort[i]);
 				k++;
 			}
 		}
 	}
-	for (i = 0; i < n; i++) {
-		multiple = 1;
-//		num_sort[i] = 0;
-		for (j = 6; j >= 0; j--) {
-			num_sort[i] += phone[i][j] * multiple;
-			multiple *= 10;
-		}
-//		cout << num_sort[i] << endl;
-	}
 
 	sort(num_sort, num_sort + n);
+//	qsort (num_sort, n, sizeof(int), (int (*) (const void *, const void *)) strcmp);
+
 #if 0
 	for (i = 0; i < n; i++) {
 		cout << num_sort[i] << endl;
 	}
 #endif
+
 	for (i = 0; i < n; i++) {
 		tmp = num_sort[i];
 		for(j = i + 1; j < n; j++) {
 			if (tmp == num_sort[j]) {
 				flag[i]++;
+				num_flag++;
 				continue;
 			}
 			break;
 		}
 		if (flag[i] > 0) {
-			cout << num_sort[i] / 10000 << "-" << num_sort[j] % 10000 << " " << flag[i] + 1 << endl;
+//			cout << num_sort[i] / 10000 << "-" << num_sort[i] % 10000 << " " << flag[i] + 1 << endl;
+			printf("%03d-%04d %d\n", num_sort[i] / 10000, num_sort[i] % 10000, flag[i] + 1);
 		}
 		i = j - 1;
 	}
+
+	if (num_flag == 0) {
+		cout << "No duplicates." << endl;
+	}
+
 	return 0;
 }
